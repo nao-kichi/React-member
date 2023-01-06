7919f6cb45530d6ffe4d85b1865b3987586a5bd0
nao-kichi <namnam3225@gmail.com>
2023-01-07 00:18:19 +0900

    README再更新

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -2,6 +2,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"; // 追加しました。
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -19,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 // Authの追加
 export const auth = getAuth(app);
+export const db = getFirestore(app)
